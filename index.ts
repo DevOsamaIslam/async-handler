e1621884d8315c734aa2a977499ea3f97af367a0
Osama <osamasamarrai@gmail.com>
2023-08-19 19:54:09 +0300

    add typing and support functions

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 export async function asyncHandler<T = any, E = Error>(
-  fn
+  fn: (() => Promise<T>) | Promise<T>
 ): Promise<[T | undefined, E | undefined]> {
   try {
-    const result = await fn
+    const result = typeof fn === "function" ? await fn() : await fn
     return [result as T, undefined]
   } catch (error) {
     return [undefined, error as E]
